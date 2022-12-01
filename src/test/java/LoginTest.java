1479260f99740b19846c20804682a583c33b037a
Evgen65 <uevgeny1965@gmail.com>
2022-12-01 20:37:44 +0200

    aaa

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -4,30 +4,18 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginTest extends TestBase {
+public class LoginTest {
    @BeforeMethod
     public void precondition(){
-   if(isLogged()){
-           logout();
+
      }
-   }
+
     @Test
     public void testLoginPositive() {
-       String email = "abcd@mail.com";
-        String password = "Abcd1234$";
-       // email=Registration.email;
-      //  password=Registration.password;
 
-        openLoginRegistration();
-        fillLoginRegistrationForm(email, password);
-        submitLogin();
-        pause(10);
-        Assert.assertNotNull(wd.findElement(By.xpath("//a[@href='/add']")));
     }
     @AfterMethod
     public void tearDown() {
-      //  wd.close();
-      //  wd.quit();
 
     }
 }
