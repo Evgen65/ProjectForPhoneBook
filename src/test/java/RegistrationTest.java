875b1e9c727e207e01af8c8a4baf5225d278d115
Evgen65 <uevgeny1965@gmail.com>
2022-12-01 15:25:34 +0200

    Update RegistrationTest.java

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -1,11 +1,10 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class Registration extends TestBase {
+public class RegistrationTest extends TestBase {
 
 
     //    WebDriver wd;
