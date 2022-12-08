9d1cc379f03a94fdd162123474dfdd1da1b053d8
Evgen65 <uevgeny1965@gmail.com>
2022-12-08 16:59:41 +0200

    new

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -13,13 +13,13 @@ public class LoginTest<T> extends TestBase {
 
   @Test
   public void testLoginPositive() {
-//      String email = "abcd@mail.com";
-//      String password = "Abcd1234$";
-//
-//      app.getUser().openLoginRegistrationForm();
-//      app.getUser().fillLoginRegistrationForm(email, password);
-//      app.getUser().submitLogin();
-//      app.getUser().pause(3);
+      String email = "abcd@mail.com";
+      String password = "Abcd1234$";
+
+      app.getUser().openLoginRegistrationForm();
+      app.getUser().fillLoginRegistrationForm(email, password);
+      app.getUser().submitLogin();
+      app.getUser().pause(3);
        app.getUser().testLoginPositive();
       Assert.assertTrue(app.getUser().isElementPresent(By.xpath("//a[@href='/add']")));
 
